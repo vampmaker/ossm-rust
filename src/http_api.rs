062bc10887e06a070da50cb127bb307771b7883b
vamp <vamp.maker@outlook.com>
2025-12-03 01:49:47 +0000

    http_api: use streaming json unmarshal

--- src/http_api.rs
+++ src/http_api.rs
@@ -1,9 +1,7 @@
-use std::sync::{Arc, Mutex};
 use esp_idf_svc::http::server::{EspHttpServer, Method};
 use serde::{Deserialize, Serialize};
-use crate::motion::{MotorControllerConfig, MotorController};
+use crate::motion::MotorControllerConfig;
 use esp_idf_svc::io::{Read, Write};
-use embedded_svc::http::Headers;
 use crate::context::AppContext;
 
 #[derive(Serialize, Deserialize)]
@@ -13,7 +11,38 @@ pub struct PausedControl {
     pub adjust: Option<f32>,               // Adjust position relatively (positive or negative)
 }
 
-const APP_HTML: &str = include_str!("../frontend/dist/index.html");
+const APP_HTML: &[u8] = include_bytes!("../frontend/dist/index.html");
+
+struct IoCompat<'a, T> {
+    inner: &'a mut T,
+    read_count: usize,
+    limit: usize,
+}
+
+impl<'a, T> IoCompat<'a, T> {
+    fn new(inner: &'a mut T, limit: usize) -> Self {
+        Self {
+            inner,
+            read_count: 0,
+            limit,
+        }
+    }
+}
+
+impl<'a, T> std::io::Read for IoCompat<'a, T>
+where
+    T: Read,
+{
+    fn read(&mut self, buf: &mut [u8]) -> std::io::Result<usize> {
+        if self.read_count >= self.limit {
+            return Err(std::io::Error::new(std::io::ErrorKind::Other, "Request too large"));
+        }
+        let max_len = buf.len().min(self.limit - self.read_count);
+        let n = self.inner.read(&mut buf[..max_len]).map_err(|e| std::io::Error::new(std::io::ErrorKind::Other, format!("{:?}", e)))?;
+        self.read_count += n;
+        Ok(n)
+    }
+}
 
 pub fn register_handlers<'a>(
     server: &mut EspHttpServer<'a>,
@@ -70,17 +99,7 @@ pub fn register_handlers<'a>(
     {
         let controller = app_context.motor_controller.clone();
         server.fn_handler::<anyhow::Error, _>("/config", Method::Post, move |mut req| {
-            let len = req.content_len().unwrap_or(0) as usize;
-            if len > 1024 {
-                req.into_response(413, None, &[("Access-Control-Allow-Origin", "*")])?
-                    .write_all("Request too big".as_bytes())?;
-                return Ok(());
-            }
-
-            let mut buf = vec![0; len];
-            req.read_exact(&mut buf)?;
-            
-            match serde_json::from_slice::<MotorControllerConfig>(&buf) {
+            match serde_json::from_reader::<_, MotorControllerConfig>(IoCompat::new(&mut req, 8192)) {
                 Ok(config) => {
                     let json = serde_json::to_string(&config).unwrap();
                     let mut mc_opt = controller.lock().unwrap();
@@ -106,17 +125,7 @@ pub fn register_handlers<'a>(
     {
         let controller = app_context.motor_controller.clone();
         server.fn_handler::<anyhow::Error, _>("/paused", Method::Post, move |mut req| {
-            let len = req.content_len().unwrap_or(0) as usize;
-            if len > 4096 {
-                req.into_response(413, None, &[("Access-Control-Allow-Origin", "*")])?
-                    .write_all("Request too big".as_bytes())?;
-                return Ok(());
-            }
-
-            let mut buf = vec![0; len];
-            req.read_exact(&mut buf)?;
-
-            match serde_json::from_slice::<PausedControl>(&buf) {
+            match serde_json::from_reader::<_, PausedControl>(IoCompat::new(&mut req, 8192)) {
                 Ok(control) => {
                     let mut mc_opt = controller.lock().unwrap();
                     if let Some(mc) = mc_opt.as_mut() {
@@ -171,7 +180,7 @@ pub fn register_handlers<'a>(
     {
         server.fn_handler::<anyhow::Error, _>("/", Method::Get, move |req| {
             req.into_response(200, Some("OK"), &[("Access-Control-Allow-Origin", "*"), ("Content-Type", "text/html")])?
-                .write_all(APP_HTML.as_bytes())?;
+                .write_all(APP_HTML)?;
             Ok(())
         }).unwrap();
     }
